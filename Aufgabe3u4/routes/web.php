7a8a11a778bbad8925ac1e25592b279717b88aff
anyhowlea <lhroro55@icloud.com>
2024-10-30 13:39:27 +0100

    anpassung routes/web.php

--- Aufgabe3u4/routes/web.php
+++ Aufgabe3u4/routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +14,22 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/hallo', function () {
+    return view('hallo');
+})->middleware(['auth', 'verified'])->name('hallo');
+
+Route::get('/job', function () {
+    return view('job');
+})->middleware(['auth', 'verified'])->name('job');
+
+Route::get('/category', function () {
+    return view('category');
+})->middleware(['auth', 'verified'])->name('category');
+
+Route::get('/company', function () {
+    return view('company');
+})->middleware(['auth', 'verified'])->name('company');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -18,3 +37,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('jobs', JobController::class);
+Route::resource('companies', CompanyController::class);
+Route::resource('categories', CategoryController::class);
