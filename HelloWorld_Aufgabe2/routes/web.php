153e35ae1c58f5baf8e13b6b5306ba3f832adb06
anyhowlea <lhroro55@icloud.com>
2024-10-30 12:10:42 +0100

    kleine Korrektur, sowie Erstellung von Job, Category, Company und User

--- HelloWorld_Aufgabe2/routes/web.php
+++ HelloWorld_Aufgabe2/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Hello World für die Webseite und Teil der Aufgabe 1:
+// Hello World für die Webseite und Teil der Aufgabe 2:
 
 Route::get('/hello', function () {
     return 'Hello World';
