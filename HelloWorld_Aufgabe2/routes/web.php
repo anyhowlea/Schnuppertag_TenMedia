219c0cb6b870bda98b5c6ca5b82fc98c9204934d
anyhowlea <lhroro55@icloud.com>
2024-10-30 11:58:17 +0100

    Hello World Aufruf

--- HelloWorld_Aufgabe2/routes/web.php
+++ HelloWorld_Aufgabe2/routes/web.php
@@ -3,18 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Hello World für die Webseite und Teil der Aufgabe 1:
+
+Route::get('/hello', function () {
+    return 'Hello World';
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
